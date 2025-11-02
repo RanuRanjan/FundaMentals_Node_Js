3284643bc0b58d3acf5acb6c4e80c0cb8ae3b44c
Ranu Ranjan <ranuranjan25@gmail.com>
2025-11-02 21:36:52 +0530

    module function inside

---
+++ moduleWrapperfunction.js
@@ -0,0 +1,8 @@
+(function (exports, require, module, __filename, __dirname) {
+ 
+})(exports, require, module, __filename, __dirname);
+
+when ever we create varible and console or use it nodejs autmatic wrap that is module wrappe function prevent not use or store directly in globally
+
+// for ex:
+a= 2
