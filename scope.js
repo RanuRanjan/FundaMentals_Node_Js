bd00cbfdeaa9b1bc642fd7bc8ab484d34102482b
Ranu Ranjan <ranuranjan25@gmail.com>
2025-11-02 21:28:01 +0530

    Started learning how add varible in global and local scope

---
+++ scope.js
@@ -0,0 +1,5 @@
+'use strict'
+
+a =6  //if use strict is not there then this varible is global
+
+globalThis.myVar="it will store global "  // instead of use strict still it will store in global variable
