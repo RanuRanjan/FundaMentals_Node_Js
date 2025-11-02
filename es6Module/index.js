dbe5d0877ea061949a1ffb5c2c969e54edecb816
Ranu Ranjan <ranuranjan25@gmail.com>
2025-11-02 22:16:59 +0530

    learnign es6 module import and export

---
+++ es6Module/index.js
@@ -0,0 +1,5 @@
+import  subs,{multiplenumber} from "./multiple.js";
+
+console.log(multiplenumber(8,9),"multiple")
+
+console.log(subs(10,1),"subs")
---
+++ es6Module/multiple.js
@@ -0,0 +1,9 @@
+export const multiplenumber=(a,b)=>{
+    return a*b
+}
+
+ const subs =(a,b)=>{
+    return a-b
+}
+
+export default subs;
